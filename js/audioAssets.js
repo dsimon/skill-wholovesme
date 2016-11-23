3f8625cdaf20c33b084625ac5628623ba01a1284
Derron Simon <dsimon@dsimon.com>
2016-11-23 14:36:22 -0500

    first commit

---
+++ js/audioAssets.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// Audio Source
+var audioData = [
+    {
+        'title' : 'Love and Civilization',
+        'url' : 'https://s3.amazonaws.com/dsimon-alexaskill/Love+and+civilization.mp3'
+    }
+];
+
+module.exports = audioData;
